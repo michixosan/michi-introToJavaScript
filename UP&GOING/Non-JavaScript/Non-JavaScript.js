35afff4b6c35a20c0ff00d91009d7bcb6f15594e
michixosan <77958432+michixosan@users.noreply.github.com>
2021-11-13 12:52:53 +0500

    Add files via upload

---
+++ UP&GOING/Non-JavaScript/Non-JavaScript.js
@@ -0,0 +1,11 @@
+
+// Non-JavaScript
+
+/** The most common non-JavaScript JavaScript you'll encounter is the DOM API */
+
+var el = document.getElementById("foo");
+
+/**
+ * the 'document' variable exists as a global variable when your code is running in a browser.
+ * It is often called "host object"
+ */
